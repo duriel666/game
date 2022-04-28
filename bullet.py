72c53e551bf09e7b9de294e8013bbc0fc0d47221
duriel666 <59049578+duriel666@users.noreply.github.com>
2022-04-29 00:05:13 +0300

    edit

--- bullet.py
+++ bullet.py
@@ -2,14 +2,14 @@ from data import *
 
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, pos, speed, enemy_image, damage, fire_rate):
+    def __init__(self, position_fired, speed, bullet_image, damage, effect):
         super().__init__()
-        self.image = pygame.image.load(enemy_image).convert_alpha()
+        self.image = pygame.image.load(bullet_image).convert_alpha()
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.width = self.image.get_width()
         self.height = self.image.get_height()
-        self.pos = vec(pos)
+        self.pos = vec(position_fired)
         self.vel = vec(speed)
         self.damage = damage
-        self.fire_rate = fire_rate
+        self.effect = effect
